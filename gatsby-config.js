8b93106dcaeca4e7921944d3defc608357b1e965
yuda1124 <yuda1124@naver.com>
2021-07-19 19:58:19 +0900

    chore: gatsby-source-filesystem, gatsby-transformer-remark 플러그인 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "young-blog",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'young-blog',
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
