ad8fd81c49319533840a4dbf14cc30123253c99d
yuda1124 <yuda1124@naver.com>
2021-07-21 17:28:49 +0900

    feat: page-header 컴포넌트 구현

---
+++ src/components/page-header/page-header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import * as styles from './page-header.module.scss';
+
+type PageHeaderProps = {
+  title: string;
+};
+
+const PageHeader = ({ title }: PageHeaderProps) => (
+  <header className={styles.header}>
+    <Link to="/">
+      <h3 className={styles.title}>{title}</h3>
+    </Link>
+  </header>
+);
+
+export { PageHeader };
