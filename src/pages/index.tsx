417a295498cf6b547b2d7a335b054408602e93c6
yuda1124 <yuda1124@naver.com>
2021-07-22 20:55:42 +0900

    feat: index 페이지 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout, PostSummaryList } from '../components';
+import { PostSummariesData } from '../types';
 
-const IndexPage = () => (
-  <div>
-    <title>Home Page</title>
-  </div>
-);
+type IndexPageProps = {
+  data: PostSummariesData;
+};
+
+const IndexPage = ({ data }: IndexPageProps) => {
+  const { allMarkdownRemark } = data;
+  const { edges: summaries } = allMarkdownRemark;
+  return (
+    <Layout>
+      <PostSummaryList {...{ summaries }} />
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query PostSummaries {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 200, truncate: true)
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
