5c2c9e0b9190cb0eef9f2aca04bf7fd7cc1c5138
yuda1124 <yuda1124@naver.com>
2021-07-22 15:06:14 +0900

    feat: post 관련 data의 type 추가

---
+++ src/types/index.ts
@@ -0,0 +1,28 @@
+type Frontmatter = {
+  title: string;
+  date: string;
+};
+
+export type BlogPostData = {
+  markdownRemark: {
+    id: string;
+    html: string;
+    frontmatter: Frontmatter;
+  };
+};
+
+export type PostSummaryData = {
+  node: {
+    id: string;
+    excerpt: string;
+    fields: {
+      slug: string;
+    };
+    frontmatter: Frontmatter;
+  };
+};
+export type PostSummariesData = {
+  allMarkdownRemark: {
+    edges: PostSummaryData[];
+  };
+};
