f2e6e060c5f16db2ea3ae2ce4a8d6bf1646ef8a7
yuda1124 <yuda1124@naver.com>
2021-07-27 01:48:50 +0900

    chore: code highlighting을 위한 gatsby-remark-prismjs 플러그인 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,36 @@ module.exports = {
             },
           },
           `gatsby-remark-autolink-headers`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: 'superscript',
+                  extend: 'javascript',
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: 'root',
+                host: 'localhost',
+                global: false,
+              },
+              escapeEntities: {},
+            },
+          },
         ],
       },
     },
