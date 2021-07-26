241ab385150da71d69061a447810aa70878912d8
yuda1124 <yuda1124@naver.com>
2021-07-27 01:49:49 +0900

    fix: 불필요한 theme import문 제거

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,5 @@
 import Typography from 'typography';
 import githubTheme from 'typography-theme-github';
-import moragaTheme from 'typography-theme-moraga';
-import wordpress2016Theme from 'typography-theme-wordpress-2016';
-import funstonTheme from 'typography-theme-funston';
 
 const typography = new Typography(githubTheme);
 
