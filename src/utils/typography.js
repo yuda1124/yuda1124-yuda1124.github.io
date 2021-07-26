2611b32524bfc81766f35a0f8efcf45010ca665b
yuda1124 <yuda1124@naver.com>
2021-07-26 21:24:42 +0900

    feat: typography.js github theme 적용

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from 'typography';
+import githubTheme from 'typography-theme-github';
+import moragaTheme from 'typography-theme-moraga';
+import wordpress2016Theme from 'typography-theme-wordpress-2016';
+import funstonTheme from 'typography-theme-funston';
+
+const typography = new Typography(githubTheme);
+
+export default typography;
