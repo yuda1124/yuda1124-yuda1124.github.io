ec29126b697d4a14d544268d999999ee567897b3
yuda1124 <yuda1124@naver.com>
2021-08-02 01:03:23 +0900

    feat: 카테고리 이동 시 중앙으로 이동하는 로직 추가

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent, useRef } from 'react';
 import * as styles from './category.module.scss';
 
 type CategoryProps = {
@@ -8,19 +8,36 @@ type CategoryProps = {
 };
 
 const Category = ({ categories, selectedCategory, setCategory }: CategoryProps) => {
+  const containerEl = useRef<HTMLDivElement>(null);
+  const changeScrollLeft = (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+    const { target } = event;
+    const { current } = containerEl;
+    if (current) {
+      const { offsetWidth: containerWidth } = current;
+      const { offsetLeft: categoryLeft, offsetWidth: categoryWidth } = target as HTMLButtonElement;
+      const centerScrollX = categoryLeft + categoryWidth / 2 - containerWidth / 2;
+      current.scroll({ left: centerScrollX >= 0 ? centerScrollX : 0 });
+    }
+  };
+  const onClick = (event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>, category: string) => {
+    changeScrollLeft(event);
+    setCategory(category);
+  };
   const renderCategories = () =>
     ['ALL', ...categories].map(category => (
       <li key={category} className={styles.line}>
         <button
           type="button"
           className={selectedCategory === category ? `${styles.btn_category} ${styles.selected}` : styles.btn_category}
-          onClick={() => setCategory(category)}>
-          {category}
+          onClick={e => {
+            onClick(e, category);
+          }}>
+          {category.toUpperCase()}
         </button>
       </li>
     ));
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={containerEl}>
       <ul className={styles.tabs}>{renderCategories()}</ul>
     </div>
   );
