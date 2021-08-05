1d2b76042e5edcb1c5a37b4370c3c117fefd85ad
yuda1124 <yuda1124@naver.com>
2021-08-05 17:28:32 +0900

    chore: gatsby browser에 글꼴 import 추가

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
+import '@fontsource/noto-sans-kr';
+import '@fontsource/abril-fatface';
+import '@fontsource/noto-serif-kr';
+
+import './src/styles/_colors.scss';
 import './src/styles/_global.scss';
 import './src/styles/_prism.scss';
 import './src/styles/_post.scss';
